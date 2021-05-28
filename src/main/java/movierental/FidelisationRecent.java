a2473b2b965ed63abe5eefdbad75f3b1316f57ce
g.saintetienne <g.saintetienne@primobox.com>
2021-05-28 11:37:37 +0200

    utilisons les mathématiques

--- src/main/java/movierental/FidelisationRecent.java
+++ src/main/java/movierental/FidelisationRecent.java
@@ -3,10 +3,6 @@ package movierental;
 public class FidelisationRecent implements FidelisationVariable {
 
     public int calculerPointDeFidelite(int dureeDeLocation) {
-        int frequentRenterPoints = 1;
-        if (dureeDeLocation > 1)
-            frequentRenterPoints++;
-
-        return frequentRenterPoints;
+        return Math.min( 2, dureeDeLocation);
     }
 }
